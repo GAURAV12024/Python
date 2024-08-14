1ea7a4bc950d86d7c01775ed458eef3f8ad5fd75
Gaurav Deshmukh <155874953+GAURAV12024@users.noreply.github.com>
2024-08-14 19:19:51 +0530

    code for pattern 2,3

---
+++ pattern 2,3/pattern1.py
@@ -0,0 +1,68 @@
+def print_upper_pyramid(x):
+    
+    if x % 2 != 0:
+        x = x + 1
+
+    x = int(x)
+    st = x // 2
+    end = x // 2
+    h = x // 2
+    
+    num = (x + 1) - h
+    num = int(num)
+
+    for i in range(num - 1):
+        
+        for a in range(x + 1):
+            if a == st or a == end:
+                print(" + ", end="")
+            else:
+                print("   ", end="")
+        print("\n")
+        st = st - 1
+        end = end + 1
+
+def print_bottom_pyramid(x):
+    
+    y=int(x)
+    if x % 2 != 0:
+        x = x + 1
+	
+    x = int(x)
+    st = 0
+    end = x
+    h = x // 2
+    
+    num = (x + 1) - h
+    num = int(num)
+    mid = x // 2
+
+    for i in range(num):
+        
+        for a in range(x + 1):
+            if a==st and a==end:
+                print(" - ", end="")
+            elif (a == st or a == end):
+                print(" + ", end="")
+            else:
+                print("   ", end="")
+        print("\n")
+        st = st + 1
+        end = end - 1
+
+def print_pattern(x):
+    
+    if x <= 1 or int(x) != x:
+        return "Enter a positive integer greater than 1"
+    
+    
+    space = x // 2
+
+    print_upper_pyramid(x)
+    print_bottom_pyramid(x)
+   
+	
+
+result = print_pattern(5)
+if result:
+    print(result)
